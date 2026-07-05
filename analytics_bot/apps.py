978374a3fe53849e058c8763070a8646774a08c9
wasemabada740-svg <wasemabada740@gmail.com>
2026-07-06 00:25:51 +0300

    Delete analytics_bot directory

--- analytics_bot/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnalyticsBotConfig(AppConfig):
-    name = 'analytics_bot'
--- analytics_bot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import chat
-
-
-urlpatterns = [
-    path('analytics/', chat, name='chat'),
-]
